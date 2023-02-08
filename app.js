b0d268609c03ec78d148b174e067ffe0f96122b8
salmannasi <124792160+salmannasi@users.noreply.github.com>
2023-02-08 16:00:33 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+
+let burger = document.querySelector(".burger");
+let links =  document.querySelector(".nav-links");
+let textarea = document.querySelector(".text-center")
+
+
+burger.addEventListener('click',()=>{
+    links.classList.toggle("nav-show");
+    textarea.classList.toggle("textareahide")
+})
